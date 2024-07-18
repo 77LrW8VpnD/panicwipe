66d3c697cbe9dc74a11bcc69f58a7ae03780f5b1
77LrW8VpnD <74686262+77LrW8VpnD@users.noreply.github.com>
2024-07-18 01:34:23 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 echo "Installing and seting up everything."
-pip install colorama django --break-system-warranty
+sudo pip install colorama django --break-system-warranty
 rm ./preview.gif ./panicwipe_logo.png ./README.md
 sudo mv panicwipe /bin/panicwipe
 sudo chmod +x /bin/panicwipe
+sudo mkdir -p /opt/config/panicwipe/
+sudo mv config.json /opt/config/panicwipe
 echo "Done!"
